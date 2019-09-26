b4e6e3f9c654e4f3a482bd63236cc27025920efb
joaoavilars <joao.avila.rs@gmail.com>
2019-09-26 09:40:48 -0300

    remover espaços da variavel de hora

--- copy.bat
+++ copy.bat
@@ -2,12 +2,13 @@
 set origem=C:\temp\teste\device
 set destino=C:\temp\teste\archive
 for /f "tokens=1-3 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%b-%%a)
-echo Data: %mydate%
 for /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a_%%b)
-echo Hora: %mytime%
+
+:: o script gera a hora e substitui o zero por espaço quando a hora for por exemplo 09:11
+:: ficando com 9:11 com espaço a frente, ocasionando erro ao renomear o arquivo.
+:: linha abaixo remove os espaços no inicio da hora
 for /f "tokens=* delims= " %%a in ("%mytime%") do set mytime=%%a
-echo Hora: %mytime%
-pause
+
 dir %origem%\ /b > list
 robocopy %origem% %destino%\
 for /f %%f in (list) do rename %destino%\\%%f %%~nf_%mydate%#%mytime%%%~xf
