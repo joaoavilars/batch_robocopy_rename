0508e3db67fe12b081aa479c627872b817ff5d85
joaoavilars <joao.avila.rs@gmail.com>
2019-12-05 18:47:20 -0300

    alterado a geração da data para ter compatibilidade com sistemas em ingles

--- copy.bat
+++ copy.bat
@@ -5,7 +5,18 @@
 
 set origem=C:\temp\teste\device
 set destino=C:\temp\teste\archive
-for /f "tokens=1-3 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%b-%%a)
+setlocal enabledelayedexpansion
+for /f "tokens=1-4 delims=/-. " %%i in ('date /t') do (
+        set v1=%%i& set v2=%%j& set v3=%%k
+        if "%%i:~0,1%%" gtr "9" (set v1=%%j& set v2=%%k& set v3=%%l)
+
+        for /f "skip=1 tokens=2-4 delims=(-)" %%m in ('echo.^|date') do (
+            set %%m=!v1!& set %%n=!v2!& set %%o=!v3!
+    )
+)
+
+set year=%yy%%aa%
+set mydate=%year%%mm%%dd%
 for /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a_%%b)
 
 :: o script gera a hora e substitui o zero por espaço quando a hora for por exemplo 09:11
@@ -15,5 +26,5 @@ for /f "tokens=* delims= " %%a in ("%mytime%") do set mytime=%%a
 
 dir %origem%\ /b > list
 robocopy %origem% "%destino%"
-for /f %%f in (list) do rename "%destino%\%%f %%~nf_%mydate%#%mytime%%%~xf"
+for /f %%f in (list) do rename "%destino%\%%f" "%%~nf_%mydate%#%mytime%%%~xf"
 del list
